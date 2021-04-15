f7032aa0f80fc08ae4fb346b5c4673495f05a060
Doruk Dorkodu <dorkodu@gmail.com>
2021-04-15 15:01:49 +0300

    A simple wrapper for Regex pattern management

--- source/Dorkodu/Utils/RegexTinkerer.php
+++ source/Dorkodu/Utils/RegexTinkerer.php
@@ -1,40 +1,42 @@
 <?php
-  /**
-   * This is an utility to using Regex patterns in a better way.
-   * @copyright (C) 2020 Doruk Dorkodu et al.
-   * @author Doruk Dorkodu <doruk@dorkodu.com>
-   * @license MIT
-   **/
-  
-  namespace Hindsight\Utils;
-  
-  class RegexTinkerer
+
+namespace Dorkodu\Utils;
+
+/**
+ * A simple wrapper for Regex pattern management
+ * Difficulty = Hardcore
+ * 
+ * @author     Doruk Eray (@dorkodu) <doruk@dorkodu.com>
+ * @copyright  (c) 2021, Doruk Eray
+ * @link       <https://github.com/dorukdorkodu/dorkodu-utils>
+ * @license    The MIT License (MIT)
+ */
+class RegexTinkerer
+{
+  protected $patterns = array();
+
+  public function setPattern($key, $pattern)
   {
-    protected $patterns = array();
-
-    public function setPattern($key, $pattern)
-    {
-      if (is_string($pattern)) {
-        $this->patterns[$key] = $pattern;
-      } else return false;     
-    }
-
-    public function getPattern($key)
-    {
-      return array_key_exists($key, $this->patterns) ? $this->patterns[$key] : false; # regex key doesnt exist
-    }
-
-    public function isPatternKey($key)
-    {
-      return array_key_exists($key, $this->patterns);
-    }
-
-    public function matchPattern($patternHolder, $content)
-    {
-      if ($this->isPatternKey($patternHolder))
-        return preg_match($this->getPattern($patternHolder), $content);
-      else
-        return preg_match($patternHolder, $content);
-    }
+    if (is_string($pattern)) {
+      $this->patterns[$key] = $pattern;
+    } else return false;
   }
-  
+
+  public function getPattern($key)
+  {
+    return array_key_exists($key, $this->patterns) ? $this->patterns[$key] : false; # regex key doesnt exist
+  }
+
+  public function isPatternKey($key)
+  {
+    return array_key_exists($key, $this->patterns);
+  }
+
+  public function matchPattern($patternHolder, $content)
+  {
+    if ($this->isPatternKey($patternHolder))
+      return preg_match($this->getPattern($patternHolder), $content);
+    else
+      return preg_match($patternHolder, $content);
+  }
+}
